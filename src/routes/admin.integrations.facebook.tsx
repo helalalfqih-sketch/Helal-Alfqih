de3a78f07b6a48d8a198d283bcb7ddc863111238
helal <helalalfqih@gmail.com>
2026-08-01 21:05:30 +0300

    fix(facebook): correct router import for createFileRoute

--- src/routes/admin.integrations.facebook.tsx
+++ src/routes/admin.integrations.facebook.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-start";
+import { createFileRoute } from "@tanstack/react-router";
 import { useState, useEffect } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useServerFn } from "@tanstack/react-start";
